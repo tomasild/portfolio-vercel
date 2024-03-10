c1f413ae6d500fc78d8a3906c62f022858f5ea13
tomasild <tomas@groovelist.co>
2024-03-09 22:28:54 -0300

    fix servicio

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const API_KEY =  import.meta.env.VITE_NEWS_API;
+const API_KEY = import.meta.env.VITE_NEWS_API;
 
 const fetchNews = async () => {
   try {
-    const response = await axios.get(API_KEY);
-    console.log(response.data.articles)
+    const response = await axios.get(`${API_KEY}`);
     return response.data.articles;
   } catch (error) {
     console.error("Error fetching news:", error);
