8cf9cfd66e1d436e0ced2faf36d1f795c8861f0a
tomasild <tomas@groovelist.co>
2024-03-10 00:26:42 -0300

    intento 2

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,5 +1,36 @@
 import axios from "axios";
 
+/* const API_KEY = "f2b8804ca23e402081c6d4eb42465a47"; */
+const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+
+const fetchNews = async () => {
+  try {
+    const response = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=us&category=technology`,
+      {
+        headers: {
+          'X-Api-Key': API_KEY,
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      return response.data.articles;
+    } else {
+      console.error(`Error fetching news. Status: ${response.status}, Message: ${response.data.message}`);
+      return [];
+    }
+  } catch (error) {
+    console.error("Error fetching news:", error.message);
+    return [];
+  }
+};
+
+export { fetchNews };
+
+
+/* import axios from "axios";
+
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
 const fetchNews = async () => {
@@ -20,3 +51,4 @@ const fetchNews = async () => {
 };
 
 export { fetchNews };
+ */
