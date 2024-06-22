fe9690282f74188de44b8386dc7fe180b78bab31
tomas.ild <tomas@groovelist.co>
2024-06-22 15:36:19 -0400

    add react image gallery

--- src/components/pages/ProjectOpen.jsx
+++ src/components/pages/ProjectOpen.jsx
@@ -1,26 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Button } from "../ui/button";
 import { Badge } from "../ui/badge";
 import { TiArrowBack } from "react-icons/ti";
 import { LuCode2, LuExternalLink } from "react-icons/lu";
 import { getProjectDataByTitle } from "@/data/projects";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 function ProjectsOpen() {
   const navigate = useNavigate();
   const { projectTitle } = useParams();
   const projectData = getProjectDataByTitle(projectTitle);
-  const [imageIndex, setImageIndex] = useState(0); // Índice de la imagen actual
 
   if (!projectData) {
     return <div>Proyecto no encontrado</div>;
   }
 
-  const images = [projectData.imagen, projectData.imagen2, projectData.imagen3];
-
-  const handleImageChange = () => {
-    setImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
+  const images = [
+    { original: projectData.imagen, thumbnail: projectData.imagen },
+    { original: projectData.imagen2, thumbnail: projectData.imagen2 },
+    { original: projectData.imagen3, thumbnail: projectData.imagen3 },
+  ];
 
   return (
     <main className="w-full md:w-1/2 lg:w-1/3 block mx-auto h-auto lg:h-[calc(100vh-2rem)] scrollbar-hide lg:overflow-auto lg:scrollbar-default bg-background rounded-md my-0 lg:my-4 dark:bg-custom_bg1 dark:hover:bg-custom_bg2">
@@ -37,23 +38,17 @@ function ProjectsOpen() {
           {projectData.title}
         </h1>
         <div className="w-full h-auto aspect-w-16 aspect-h-9 tracking-wider text-pretty">
-          <img
-            src={images[imageIndex]}
-            alt={`Imagen ${imageIndex + 1}`}
-            className="w-full h-full object-contain cursor-pointer aspect-video mt-4"
-            onClick={handleImageChange}
+          <ImageGallery
+            items={images}
+            showThumbnails={false}
+            showPlayButton={true}
+            showFullscreenButton={true}
+            showBullets={true}
+            showNav={false}
+            autoPlay={true}
+            slideInterval={3000}
+            slideDuration={400}
           />
-          <div className="flex justify-center mt-2">
-            {[0, 1, 2].map((index) => (
-              <div
-                key={index}
-                className={`w-4 mx-2 h-4 bg-accent rounded-full cursor-pointer ${
-                  index === imageIndex ? "bg-primary" : ""
-                }`}
-                onClick={() => setImageIndex(index)}
-              />
-            ))}
-          </div>
         </div>
         <section className="py-2">
           {[
@@ -76,7 +71,6 @@ function ProjectsOpen() {
             alt="vista de la app en un dispositivo móvil"
           />
         )}
-
         <section className="py-2">
           {[projectData.descripcion4, projectData.descripcion5].map(
             (descripcion, index) => (
